420115011a212c8637e0ff673ccff9975ebec748
kike <ea.ortizalfaro@ugto.mx>
2024-05-02 13:26:14 -0600

    Se actualizó la vista de la creación de usuario, falta backend

--- FRONTEND/layouts/default.vue
+++ FRONTEND/layouts/default.vue
@@ -1,60 +1,39 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app app>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      elevation="0"
+      color="black"
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-toolbar-title style="color: white;" class="fuente">
+        {{ title }}
+      </v-toolbar-title>
       <v-spacer />
       <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+        large
+        plain
+        color="white"
+        class="fuente"
       >
-        <v-icon>mdi-menu</v-icon>
+        {{ item.title }}
+      </v-btn>
+      <v-spacer />
+      <v-btn
+        rounded
+        x-large
+        elevation="0"
+        color="white"
+        class="fuente"
+        style="color: black;"
+      >
+        Cuenta
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -62,29 +41,22 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
+    <!-- Botón para cambiar el tema -->
+    <v-btn
+      icon
+      dark
+      color="black"
+      class="ma-4"
+      bottom
+      right
       fixed
+      elevation="0"
+      @click="toggleDarkTheme"
     >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+      <v-icon color="grey">
+        mdi-lightbulb-outline
+      </v-icon>
+    </v-btn>
   </v-app>
 </template>
 
@@ -98,24 +70,19 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          icon: 'mdi-car-side',
+          title: 'Menú Principal',
+          to: '/principal/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        },
-        { // Aqui añadir los iconos para las diferentes pestañas!
           icon: 'mdi-car-info',
-          title: 'reg_vehiculo',
-          to: '/reg_vehiculo'
+          title: 'Registrar Vehículo',
+          to: '/principal/reg_vehiculo/'
         },
         {
           icon: 'mdi-card-account-details-outline',
-          title: 'Registro_conductor',
-          to: '/reg_usuario'
+          title: 'Registrar usuario',
+          to: '/principal/reg_usuario/'
         }
       ],
       miniVariant: false,
@@ -123,6 +90,18 @@ export default {
       rightDrawer: false,
       title: 'Raites DICIS'
     }
+  },
+  methods: {
+    toggleDarkTheme () {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    }
   }
 }
 </script>
+
+<style>
+.fuente {
+  font-weight: bold;
+  font-family: Source Sans Pro;
+}
+</style>
